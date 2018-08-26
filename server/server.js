b1a1371feb6323b8532c36a605d1d328b5ffda2d
jsina <jsina.maleki@gmail.com>
2018-08-26 17:30:28 +0430

    add env file

--- server/server.js
+++ server/server.js
@@ -4,17 +4,18 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
+require('dotenv').config()
 
 const app = express();
-
+console.log(process.env.DB_USER,'fdslj')
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds125342.mlab.com:25342/persian-lyrics`;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, { useMongoClient: true });
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
